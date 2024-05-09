e2d0efaf3244ab059aab69f7e87003da6ca0e9b5
Tim Mitra <tim@it-guy.com>
2024-05-09 13:27:32 -0400

    Initial Commit

---
+++ NavigationSplitView/NavigationSplitViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationSplitViewApp.swift
+//  NavigationSplitView
+//
+//  Created by Tim Mitra on 2024-05-09.
+//
+
+import SwiftUI
+
+@main
+struct NavigationSplitViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
