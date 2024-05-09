94e6060166623416055d25bd3e31c30b39da9f74
Tim Mitra <tim@it-guy.com>
2024-05-09 13:43:49 -0400

    add loops

--- NavigationSplitView/RootView.swift
+++ NavigationSplitView/RootView.swift
@@ -8,16 +8,58 @@
 import SwiftUI
 
 struct RootView: View {
-    var body: some View {
-      // two column
-        NavigationSplitView {
-            // side bar
-        } detail: {
-           // item details
+  @State private var selectedFolder: String?
+  @State private var selectedItem: String?
+  
+  @State private var folders = [
+    "All": [
+      "Item1",
+      "Item2"
+    ],
+    "Favorites": [
+      "Item2"
+    ]
+  ]
+  
+  var body: some View {
+    // two column
+    NavigationSplitView {
+      // side bar
+      List(selection: $selectedFolder) {
+        ForEach(Array(folders.keys.sorted()), id: \.self) { folder in
+          NavigationLink(value: folder) {
+            Text(verbatim: folder)
+          }
         }
+      }
+      .navigationTitle("Sidebar")
+    } content: {
+      if let selectedFolder {
+        List(selection: $selectedItem) {
+          ForEach(folders[selectedFolder, default: []], id: \.self) { item in
+            NavigationLink(value: item) {
+              Text(verbatim: item)
+            }
+          }
+        }
+        .navigationTitle(selectedFolder)
+      } else {
+        Text("Choose a folder from the sidebar")
+      }
+    } detail: {
+      // item details
+      if let selectedItem {
+        NavigationLink(value: selectedItem) {
+          Text(verbatim: selectedItem)
+            .navigationTitle(selectedItem)
+        }
+      } else {
+        Text("Choose an item from the sidebar")
+      }
     }
+  }
 }
 
 #Preview {
-    RootView()
+  RootView()
 }
